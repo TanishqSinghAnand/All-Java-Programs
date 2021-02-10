6b12e9985084a0f1eeb0828cd15ddd8581ca9425
TanishqSinghAnand <anandtanishqs@gmail.com>
2021-02-10 11:07:23 +0530

    Did Homework Programs

---
+++ Returning.java
@@ -0,0 +1,13 @@
+class Returning
+{
+    int sum(int first , int second){
+        int sum = first + second ;
+        return sum;
+    }
+    void main(int f , int s , int t , int fr){
+        int first = this.sum(f,s);
+        int second = this.sum(t,fr);
+        int mul = first * second;
+        System.out.println("Multiply = " + mul);
+    }
+}
