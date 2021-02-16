8f8e440434c088b1428fe6da391cdbab15a9ec62
TanishqSinghAnand <anandtanishqs@gmail.com>
2021-02-16 09:04:24 +0530

    Added Codes

---
+++ MinsToHrsAndMin.java
@@ -0,0 +1,12 @@
+
+public class MinsToHrsAndMin {
+    void min(long min) {
+
+        long hrs = min / 60;
+        long mins = min % 60;
+        System.out.println("Number of Hours are " + hrs);
+        System.out.println("Number of Mins are " + mins);
+
+    }
+
+}
---
+++ Volume.java
@@ -0,0 +1,6 @@
+public class Volume{
+    void cube(double side){
+        double volume = Math.pow(side,3);
+        System.out.println("Volume of the cube is = " + volume);
+    }
+}
